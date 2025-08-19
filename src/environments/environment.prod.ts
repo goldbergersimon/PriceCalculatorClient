4254de3b91ee3ea02d6b164f00deda70e836a6c1
Joseph Goldberger <joseph@jlfulfillment.com>
2025-08-19 13:07:04 -0400

    bug fixes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://pricecalculator-e3dybwbafydea7h0.eastus2-01.azurewebsites.net/api',
+};
--- src/environments/environments.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'pricecalculator-e3dybwbafydea7h0.eastus2-01.azurewebsites.net/api',
-};
