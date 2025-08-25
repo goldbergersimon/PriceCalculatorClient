7aa52ad8b280569f816d6a3cfc7622a9641e25c0
Joseph Goldberger <joseph@jlfulfillment.com>
2025-08-25 16:54:51 -0400

    it works?

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { locale } from 'devextreme/localization';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+locale('en-US');
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
